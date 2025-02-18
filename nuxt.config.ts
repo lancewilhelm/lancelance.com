e1428cb57f07e9759914461a2f3dcf0bfda546b4
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-02-18 15:27:57 -0500

    adding an apple favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
       ]
     }
   },
