29b4977eae1ee410de796b70c2b228cf8a1393f5
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-03-03 11:12:25 -0500

    adding github link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,15 +4,20 @@ import { useState } from 'react'
 import Modal from '@/components/Modal'
 import ThemePicker from "./ThemePicker";
 import PaletteIcon from '@/components/icons/Palette'
+import GithubIcon from '@/components/icons/Github'
+import Link from 'next/link'
 
 export default function Footer() {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
-    <footer className="flex justify-center items-center col-start-[content-start] row-start-[footer-start]">
+    <footer className="flex gap-2 justify-center items-center col-start-[content-start] row-start-[footer-start]">
       <div className="icon cursor-pointer" onClick={() => setIsModalOpen(true)}>
         <PaletteIcon fill="var(--main-color)" />
       </div>
+      <Link href='https://github.com/lancewilhelm'>
+        <GithubIcon fill="var(--main-color)" />
+      </Link>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <ThemePicker />
       </Modal>
