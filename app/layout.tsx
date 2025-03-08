cabc0a31599e7bf6580a094c90c159e7c15de0aa
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-03-07 20:53:20 -0500

    observing user color scheme preference for new visitors

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,15 +10,18 @@ interface LayoutProps {
 }
 
 export default async function DefaultLayout({ children }: LayoutProps) {
-  const theme = (await cookies()).get('theme')?.value || 'monochrome'
-  const themeHref = `/css/themes/${theme}.css`
-  const faviconHref = generateFaviconSVG(theme) // Generate favicon on the server
+  const theme = (await cookies()).get('theme')?.value
+  let themeHref, faviconHref
+  if (theme) {
+    themeHref = `/css/themes/${theme}.css`
+    faviconHref = generateFaviconSVG(theme) // Generate favicon on the server
+  }
 
   return (
     <html lang="en">
       <head>
-        <link id='currentTheme' rel='stylesheet' type='text/css' href={themeHref} />
-        <link id="favicon" rel="icon" type="image/svg+xml" href={faviconHref} />
+        {theme && <link id='currentTheme' rel='stylesheet' type='text/css' href={themeHref} />}
+        {theme && <link id="favicon" rel="icon" type="image/svg+xml" href={faviconHref} />}
       </head>
       <body className={`m-0 bg-[--bg-color] text-[--text-color] overflow-x-hidden text-[14pt] ${geistSans.className}`}>
         {children}
--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -9,9 +9,9 @@ export function useTheme() {
   const getCookieTheme = () => {
     if (typeof document !== 'undefined') {
       const match = document.cookie.match(new RegExp('(^| )' + THEME_STORAGE_KEY + '=([^;]+)'))
-      return match ? match[2] : 'monochrome'
+      return match ? match[2] : undefined
     }
-    return 'monochrome'
+    return undefined
   }
 
   const [currentTheme, setCurrentTheme] = useState(getCookieTheme)
@@ -24,8 +24,10 @@ export function useTheme() {
       }
 
       // Set theme cookie
-      loadThemeCSS(currentTheme)
-      document.cookie = `${THEME_STORAGE_KEY}=${currentTheme}; path=/; max-age=31536000` // 1 year expiry
+      if (currentTheme) {
+        loadThemeCSS(currentTheme)
+        document.cookie = `${THEME_STORAGE_KEY}=${currentTheme}; path=/; max-age=31536000` // 1 year expiry
+      }
     }
   }, [currentTheme])
 
