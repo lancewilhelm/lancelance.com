8cdd23c7e178ec22b26e3673dd75f54b7d226245
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-10-09 09:14:19 -0400

    feat: Add GitHub link to footer with icon

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -3,10 +3,20 @@ const showThemePicker = ref(false);
 </script>
 
 <template>
-  <footer class='flex row-start-[content-end] col-start-[content-start] justify-center items-center'>
-    <div class="cursor-pointer" @click="showThemePicker = true">
-      <Icon name="fa6-solid:palette" class="text-(--sub-color)" />
-    </div>
-    <ThemePicker v-if="showThemePicker" @close="showThemePicker = false" />
-  </footer>
+    <footer
+        class="flex row-start-[content-end] col-start-[content-start] justify-center items-center"
+    >
+        <div
+            class="cursor-pointer flex gap-1 items-center"
+            @click="showThemePicker = true"
+        >
+            <Icon name="fa6-solid:palette" class="text-(--sub-color)" />
+            <NuxtLink
+                class="flex items-center"
+                to="https://github.com/lancewilhelm"
+                ><Icon name="simple-icons:github" class="text-(--sub-color)"
+            /></NuxtLink>
+        </div>
+        <ThemePicker v-if="showThemePicker" @close="showThemePicker = false" />
+    </footer>
 </template>
